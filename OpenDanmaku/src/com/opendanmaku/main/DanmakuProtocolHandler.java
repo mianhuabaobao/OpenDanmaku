f858fe208502f99ed9346338fe45c605c3449f2f
mianhuabaobao <mianhuabaobao@foxmail.com>
2017-05-18 14:57:02 +0800

    ?

--- OpenDanmaku/src/com/opendanmaku/main/DanmakuProtocolHandler.java
+++ OpenDanmaku/src/com/opendanmaku/main/DanmakuProtocolHandler.java
@@ -1,63 +1,94 @@
 package com.opendanmaku.main;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
-import org.apache.mina.filter.ssl.SslFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.opendanmaku.codec.MessageRequest;
+import com.opendanmaku.util.MessageConstants;
 
 public class DanmakuProtocolHandler extends IoHandlerAdapter {
     private final static Logger LOGGER = LoggerFactory.getLogger(DanmakuProtocolHandler.class);
     
-    private AtomicInteger mClients = new AtomicInteger(0);
+    private final Set<IoSession> sessions = Collections.synchronizedSet(new HashSet<IoSession>());
+    private final Set<String> users = Collections.synchronizedSet(new HashSet<String>());
     
     @Override
     public void sessionCreated(IoSession session) {
-        //session.getConfig().setIdleTime(IdleStatus.BOTH_IDLE, 15);
 
-        // We're going to use SSL negotiation notification.
-        session.setAttribute(SslFilter.USE_NOTIFICATION);
-        int clients = mClients.incrementAndGet();
-        System.out.println("sessionCreated >>> " + clients);
     }
     
     @Override
     public void sessionOpened(IoSession session) throws Exception {
-        LOGGER.info("OPENED");
-        System.out.println("sessionOpened");        
+   
     }
 
     @Override
     public void sessionClosed(IoSession session) throws Exception {
-        LOGGER.info("CLOSED");
-        int clients = mClients.decrementAndGet();
-        System.out.println("sessionClosed >>> " + clients);
+        String user = (String) session.getAttribute("user");
+        users.remove(user);
+        sessions.remove(session);
+        //broadcast();
     }
 
     @Override
     public void sessionIdle(IoSession session, IdleStatus status) {
-        LOGGER.info("*** IDLE #" + session.getIdleCount(IdleStatus.READER_IDLE) + " ***");
         System.out.println("*** sessionIdle #" + session + " #" + session.getIdleCount(IdleStatus.READER_IDLE) + " ***");
         //session.closeNow();
     }
 
     @Override
     public void exceptionCaught(IoSession session, Throwable cause) {
-    	cause.printStackTrace();
+        LOGGER.warn("Unexpected exception.", cause);
+        // Close connection when unexpected exception is caught.
         session.closeNow();
-        System.out.println("exceptionCaught"); 
     }
-
-    private int count = 0;
     
     @Override
     public void messageReceived(IoSession session, Object message) throws Exception {
-        System.out.println("messageReceived : count = " + (++count) + " | " + message);
-        // Write the received data back to remote peer
-        //session.write(((IoBuffer) message).duplicate());
-        session.write(message);
+    	MessageRequest request = (MessageRequest) message;
+
+        switch (request.getType()) {
+        case MessageConstants.MESSAGE_SUBSCRIBE:
+        	
+        	break;
+        case MessageConstants.MESSAGE_BROADCAST:
+        	
+        	break;
+        default:
+        	break;
+        }
+
+    }
+    
+    public void broadcast(String message) {
+        synchronized (sessions) {
+            for (IoSession session : sessions) {
+                if (session.isConnected()) {
+                    session.write("BROADCAST OK " + message);
+                }
+            }
+        }
+    }
+
+    public void kick(String name) {
+        synchronized (sessions) {
+            for (IoSession session : sessions) {
+                if (name.equals(session.getAttribute("user"))) {
+                    session.closeNow();
+                    break;
+                }
+            }
+        }
+    }
+    
+    public int getUsersSize() {
+        return users.size();
     }
 }
